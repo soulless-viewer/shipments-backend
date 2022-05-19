a3995a8826d96e1503a55bdaefed42dc8cabc5f4
Mikalai Lisitsa <Mikalai.Lisitsa1@ibm.com>
2022-05-19 22:13:01 +0400

    fix: make Meta.fields immutable

--- shipment/serializers.py
+++ shipment/serializers.py
@@ -5,11 +5,11 @@ from shipment.models import Shipment
 class ShipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shipment
-        fields = [
+        fields = (
             "id",
             "arrival_dt",
             "departure_dt",
             "cargo_volume",
             "arrival_point",
             "departure_point",
-        ]
+        )
